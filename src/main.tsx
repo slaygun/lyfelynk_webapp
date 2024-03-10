373a5bb6f3fe8f5d425572e9f5da55a00ca061f3
slaygun <nabhraghav@gmail.com>
2024-03-10 10:06:38 +0530

    location change

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.tsx'
+import App from './onboarding/App.tsx'
 import './index.css'
 
 const router = createBrowserRouter([
